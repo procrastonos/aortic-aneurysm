b6854edcd36c354468439fccade85cab5667ed52
procrastonos <procrastonos@online.de>
2014-01-16 08:59:45 +0100

    Change code to use image array

--- aneurysm_Jan.m
+++ aneurysm_Jan.m
@@ -79,14 +79,23 @@ function next_Button_Callback(hObject, eventdata, handles)
 % hObject    handle to next_Button (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
-av_files2 = handles.av_files;
-path2 = handles.path;
+
+% get current image id
 imCount = handles.imCount;
-name = av_files2(imCount,1).name;
-img = dicomread(fullfile(path2, name));
-imshow(img, []);
-handles.img = img;
+
+% get image
+im = handles.img(:, :, imCount + 1);
+
+% select axes
+axes(handles.OrigImg);
+
+% show image
+imshow(im, []);
+
+% increase image count
 imCount = imCount + 1;
+
+% update handles
 handles.imCount = imCount;
 guidata(hObject, handles);
 
@@ -98,19 +107,42 @@ function read_Button_Callback(hObject, eventdata, handles)
 % handles    structure with handles and user data (see GUIDATA)
 % av_files = dir(fullfile(path,'*.dcm'));
 % read DICOM image
-[file, path]=uigetfile('C:\Users\JanHenric\SkyDrive\Uni\Uebungen\S5\MedBV\Aortic_aneurysm\4\*.dcm*','Select the file to read');
-av_files = dir(fullfile(path,'*.dcm'));  
-img=dicomread(fullfile(path,file));    %to reactivate
-%img = imread(fullfile(path, file)); %to remove
-%img = rgb2gray(img);
-%pinfo=dicominfo(fullfile(path,file)); 
-axes(handles.OrigImg);
-imshow(img, []);
-handles.img=img;
-handles.av_files = av_files;
-%handles.pinfo=pinfo;
-handles.path = path;
 
+% get folder containing dicom series from user
+%folder = uigetdir('C:\Users\JanHenric\SkyDrive\Uni\Uebungen\S5\MedBV\Aortic_aneurysm\4\*.dcm*','Select folder');
+folder = uigetdir('./data/','Select folder');
+files = dir(fullfile(folder,'*.dcm'));
+
+% create handle for files (probably no longer needed though)
+handles.files = files;
+
+img = [];
+pinfo = [];
+
+% read images
+for k=180:210 % TODO size(files, 1)
+    filename = files(k, 1).name;
+    im = dicomread(fullfile(folder, filename));
+    pi = dicominfo(fullfile(folder, filename));
+    
+    img = cat(3, img, im);
+    pinfo = cat(1, pinfo, pi);
+end
+
+% create handle for images and patient info
+handles.img = img;
+handles.pinfo = pinfo;
+
+% select axes
+axes(handles.OrigImg);
+
+% select image
+im = img(:, :, 1);
+
+% show image
+imshow(im, []);
+
+% activate GUI elements
 set(handles.threshold_Button, 'Enable', 'on');
 set(handles.circle_Button, 'Enable', 'on');
 set(handles.labeling_Button, 'Enable', 'on');
@@ -120,6 +152,8 @@ set(handles.edge_Button, 'Enable', 'on');
 set(handles.distance_Button, 'Enable', 'on');
 set(handles.erosion_Button, 'Enable', 'on');
 set(handles.next_Button, 'Enable', 'on');
+
+% update hObject with new handles
 guidata(hObject, handles);
 %%
 
@@ -129,12 +163,24 @@ function threshold_Button_Callback(hObject, eventdata, handles)
 % hObject    handle to threshold_Button (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
+
+% get threshold value
 tr = handles.tr;
-trmask=handles.img;
-trmask(trmask<=tr)=0;
-axes(handles.OrigImg);
-imshow(trmask,[]);
-handles.img=trmask;
+
+% select image
+trmask = handles.img(:, :, handles.imCount);
+
+% apply threshold
+trmask(trmask <= tr) = 0;
+
+% select axes
+axes(handles.ResImg);
+
+% show image
+imshow(trmask, []);
+
+% update handles
+handles.thresh = trmask;
 guidata(hObject, handles);
 
 
@@ -143,49 +189,63 @@ function labeling_Button_Callback(hObject, eventdata, handles)
 % hObject    handle to labeling_Button (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
-img = handles.img;
+
+% select image
+im = handles.img(:, :, handles.imCount);
+
+% get user input
 [y,x] = ginput(1);
 
-gr = img(round(x), round(y));
+% calculate pixel position of user input
+gr = im(round(x), round(y));
+
 % global criteria
 tr1 = gr-0.2*gr;
 tr2 = gr+0.2*gr;
 
 % perform the thresholding operation
-data=zeros(size(img));
-data(img>=tr1&img<=tr2)=1;
+data = zeros(size(im));
+data(im >= tr1 & im <= tr2) = 1;
 
+% perform labeling
 bl = bwlabeln(data, 4);
-axes(handles.OrigImg);
-imshow(bl, []);
-lab = bl(round(x),round(y));
+lab = bl(round(x), round(y));
 bll = bl;
-bll(bll~=lab)=0;
-bll(bll~=0)=1;
+bll(bll ~= lab) =0;
+bll(bll ~= 0) =1;
+
+% select axes
 axes(handles.ResImg);
+
+% show image
 imshow(bll, []);
-%figure; imshow(bll,[]);
-
-
 
 % --- Executes on button press in circle_Button.
 function circle_Button_Callback(hObject, eventdata, handles)
 % hObject    handle to circle_Button (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
-img = handles.img;
+
+% select image
+im = handles.img(:, :, handles.imCount);
+
+% get sensitivity value
 sensitivity = handles.sensitivity;
+
+% get radius
 radius = handles.radius;
-[centers,radii] = imfindcircles(img, [radius - 8 radius + 8],'Sensitivity',sensitivity);
-axes(handles.OrigImg);
-imshow(img, []);
-viscircles(centers,radii);
-handles.img = img;
-guidata(hObject, handles);
 
+% perform circle matching
+[centers,radii] = imfindcircles(im, [radius - 8 radius + 8], 'Sensitivity', sensitivity);
 
+% select axes
+axes(handles.ResImg);
 
+% show image
+imshow(im, []);
 
+% draw circles
+viscircles(centers, radii);
 
 function tr_Edit_Callback(hObject, eventdata, handles)
 % hObject    handle to tr_Edit (see GCBO)
@@ -194,10 +254,13 @@ function tr_Edit_Callback(hObject, eventdata, handles)
 
 % Hints: get(hObject,'String') returns contents of tr_Edit as text
 %        str2double(get(hObject,'String')) returns contents of tr_Edit as a double
-tr = str2double(get(hObject, 'String'));
-handles.tr = tr*max(handles.img(:));
-guidata(hObject, handles);
 
+% get value of input field
+tr = str2double(get(hObject, 'String'));
+
+% update handles
+handles.tr = tr * max(handles.img(:, :, handles.imCount));
+guidata(hObject, handles);
 
 % --- Executes during object creation, after setting all properties.
 function tr_Edit_CreateFcn(hObject, eventdata, handles)
@@ -210,9 +273,10 @@ function tr_Edit_CreateFcn(hObject, eventdata, handles)
 if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
     set(hObject,'BackgroundColor','white');
 end
+
+% set initial value
 handles.tr = 0;
 guidata(hObject, handles);
-
 
 % --- Executes on button press in dilation_Button.
 function dilation_Button_Callback(hObject, eventdata, handles)
@@ -220,7 +284,10 @@ function dilation_Button_Callback(hObject, eventdata, handles)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
 % 2D dilation
-img = handles.img;
+
+% get image
+im = handles.img(:, :, handles.imCount);
+
 %tr = handles.tr;
 %se = strel('disk', 1);
 %img(img <= tr) = 0;
@@ -234,58 +301,76 @@ img = handles.img;
 se3(:,:,1) = [0 0 0;0 1 0; 0 0 0];
 se3(:,:,2) = [0 1 0;1 1 1; 0 1 0];
 se3(:,:,3) = [0 0 0;0 1 0; 0 0 0];
-se3
-img_dl3 = imdilate(img, se3);
-axes(handles.OrigImg);
+
+% dilate image
+img_dl3 = imdilate(im, se3);
+
+% select axes
+axes(handles.ResImg);
+
+% show image
 imshow(img_dl3, []);
+
+% update handles
 handles.img = im_dl3;
 guidata(hObject, handles);
-
-
 
 % --- Executes on button press in goto_Button.
 function goto_Button_Callback(hObject, eventdata, handles)
 % hObject    handle to goto_Button (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
-av_files = handles.av_files;
-path = handles.path;
-name = av_files(200,1).name;
-img = dicomread(fullfile(path, name));
+
+% select axes
 axes(handles.OrigImg);
-imshow(img, []);
+
+% set image to 200
 handles.imCount = 200;
-handles.img = img;
+im = handles.img(:, :, handles.imCount);
+
+% show image
+imshow(im, []);
+
+% update handles
 guidata(hObject, handles);
-
-
 
 % --- Executes on button press in edge_Button.
 function edge_Button_Callback(hObject, eventdata, handles)
 % hObject    handle to edge_Button (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
-img = handles.img;
+
+% select image
+im = handles.img(:, :, handles.imCount);
+
+% select edge detection filter
 h = fspecial('Prewitt');
-imfil = filter2(h,img,'same');
-axes(handles.OrigImg);
+
+% apply edge detection filter
+imfil = filter2(h, im, 'same');
+
+% select output axes
+axes(handles.ResImg);
+
+% show image
 imshow(imfil, []);
-handles.img = imfil;
-guidata(hObject, handles);
-
-
 
 % --- Executes on button press in distance_Button.
 function distance_Button_Callback(hObject, eventdata, handles)
 % hObject    handle to distance_Button (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
+
+% get two user input positions
 [x,y] = ginput(2);
+
+% calculate distance between points
 distanceX = max(x) - min(x);
 distanceY = max(y) - min(y);
 distance = sqrt(distanceX^2 + distanceY^2);
-set(handles.distance_EditText, 'String', distance);
 
+% output distance
+set(handles.distance_EditText, 'String', distance);
 
 % --- Executes on button press in erosion_Button.
 function erosion_Button_Callback(hObject, eventdata, handles)
@@ -293,18 +378,23 @@ function erosion_Button_Callback(hObject, eventdata, handles)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
 % 3D erosion
-img = handles.img;
+
+% select image
+im = handles.img(:, :, handles.imCount);
+
+% create an erosion mask
 se3(:,:,1)=[0 0 0;0 1 0;0 0 0];
 se3(:,:,2)=[0 1 0;1 1 1;0 1 0];
 se3(:,:,3)=[0 0 0;0 1 0;0 0 0];
 
-img_er3 = imerode(img,se3);
-axes(handles.OrigImg);
+% erode image
+img_er3 = imerode(im, se3);
+
+% select output axes
+axes(handles.ResImg);
+
+% show image
 imshow(img_er3, []);
-handles.img = img_er3;
-guidata(hObject, handles);
-
-
 
 function sensitivity_Edit_Callback(hObject, eventdata, handles)
 % hObject    handle to sensitivity_Edit (see GCBO)
@@ -313,7 +403,11 @@ function sensitivity_Edit_Callback(hObject, eventdata, handles)
 
 % Hints: get(hObject,'String') returns contents of sensitivity_Edit as text
 %        str2double(get(hObject,'String')) returns contents of sensitivity_Edit as a double
+
+% read sensitivity from input field
 sensitivity = str2double(get(hObject, 'String'));
+
+% update handles
 handles.sensitivity = sensitivity;
 guidata(hObject, handles);
 
@@ -328,9 +422,10 @@ function sensitivity_Edit_CreateFcn(hObject, eventdata, handles)
 if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
     set(hObject,'BackgroundColor','white');
 end
+
+% set initial sensitivity value
 handles.sensitivity = 0;
-
-
+guidata(hObject, handles);
 
 function radius_Edit_Callback(hObject, eventdata, handles)
 % hObject    handle to radius_Edit (see GCBO)
@@ -339,7 +434,11 @@ function radius_Edit_Callback(hObject, eventdata, handles)
 
 % Hints: get(hObject,'String') returns contents of radius_Edit as text
 %        str2double(get(hObject,'String')) returns contents of radius_Edit as a double
-radius = str2double(get(hObject, 'String'));
+
+% get radius from input field
+radius = round(str2double(get(hObject, 'String')));
+
+% update handles
 handles.radius = radius;
 guidata(hObject, handles);
 
@@ -354,3 +453,7 @@ function radius_Edit_CreateFcn(hObject, eventdata, handles)
 if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
     set(hObject,'BackgroundColor','white');
 end
+
+% set initial radius
+handles.radius = 0;
+guidata(hObject, handles);
