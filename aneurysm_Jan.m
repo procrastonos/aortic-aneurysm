2460782281ef358961d242af1d8fa585de230684
procrastonos <procrastonos@online.de>
2014-01-20 15:34:43 +0100

    Add function for level-set algorithm

--- aneurysm_Jan.m
+++ aneurysm_Jan.m
@@ -22,7 +22,7 @@ function varargout = aneurysm_Jan(varargin)
 
 % Edit the above text to modify the response to help aneurysm_Jan
 
-% Last Modified by GUIDE v2.5 14-Jan-2014 10:40:47
+% Last Modified by GUIDE v2.5 16-Jan-2014 09:20:35
 
 % Begin initialization code - DO NOT EDIT
 gui_Singleton = 1;
@@ -61,7 +61,6 @@ guidata(hObject, handles);
 
 % UIWAIT makes aneurysm_Jan wait for user response (see UIRESUME)
 % uiwait(handles.window);
-
 
 % --- Outputs from this function are returned to the command line.
 function varargout = aneurysm_Jan_OutputFcn(hObject, eventdata, handles) 
@@ -120,7 +119,7 @@ img = [];
 pinfo = [];
 
 % read images
-for k=180:210 % TODO size(files, 1)
+for k = 198:202 % TODO size(files, 1)
     filename = files(k, 1).name;
     im = dicomread(fullfile(folder, filename));
     pi = dicominfo(fullfile(folder, filename));
@@ -152,11 +151,11 @@ set(handles.edge_Button, 'Enable', 'on');
 set(handles.distance_Button, 'Enable', 'on');
 set(handles.erosion_Button, 'Enable', 'on');
 set(handles.next_Button, 'Enable', 'on');
+set(handles.levelset_Button, 'Enable', 'on');
 
 % update hObject with new handles
 guidata(hObject, handles);
 %%
-
 
 % --- Executes on button press in threshold_Button.
 function threshold_Button_Callback(hObject, eventdata, handles)
@@ -182,7 +181,6 @@ imshow(trmask, []);
 % update handles
 handles.thresh = trmask;
 guidata(hObject, handles);
-
 
 % --- Executes on button press in labeling_Button.
 function labeling_Button_Callback(hObject, eventdata, handles)
@@ -457,3 +455,104 @@ end
 % set initial radius
 handles.radius = 0;
 guidata(hObject, handles);
+
+
+% --- Executes on button press in levelset_Button.
+function levelset_Button_Callback(hObject, eventdata, handles)
+% hObject    handle to levelset_Button (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% select image
+im = handles.img(:, :, handles.imCount);
+
+% select output axes
+axes(handles.ResImg);
+
+%% parameter setting
+timestep = 5;  % time step
+mu = 0.2 / timestep;  % coefficient of the distance regularization term R(phi)
+iter_inner = 5;
+iter_outer = 40;
+lambda = 5; % coefficient of the weighted length term L(phi)
+alfa = 1.5;  % coefficient of the weighted area term A(phi)
+epsilon = 1.5; % papramater that specifies the width of the DiracDelta function
+sigma = 1.5;     % scale parameter in Gaussian kernel
+
+G = fspecial('gaussian', 15, sigma);
+
+axes(handles.ResImg);
+
+Img_smooth = conv2(im, G, 'same');  % smooth image by Gaussiin convolution
+
+imshow(Img_smooth, []);
+
+[Ix,Iy] = gradient(Img_smooth);
+f = Ix .^ 2 + Iy .^ 2;
+g = 1 ./ (1 + f);  % edge indicator function.
+
+% initialize LSF as binary step function
+c0 = 2;
+initialLSF = c0 * ones(size(im));
+
+% get user input
+[x,y] = ginput(2);
+
+axes(handles.OrigImg);
+
+rectangle('Position',[min(x), min(y), max(x) - min(x), max(y) - min(y)])
+
+axes(handles.ResImg);
+
+% generate the initial region R0 as a rectangle
+initialLSF(min(x):min(y), max(x):max(y)) = -c0;  
+phi = initialLSF;
+
+mesh(-phi);   % for a better view, the LSF is displayed upside down
+hold on;  contour(phi, [0,0], 'r','LineWidth',2);
+title('Initial level set function');
+view([-80 35]);
+
+pause(2);
+
+imagesc(im,[0, 255]); axis off; axis equal; colormap(gray); hold on;  contour(phi, [0,0], 'r');
+title('Initial zero level contour');
+pause(0.5);
+
+potential=2;  
+if potential ==1
+    potentialFunction = 'single-well';  % use single well potential p1(s)=0.5*(s-1)^2, which is good for region-based model 
+elseif potential == 2
+    potentialFunction = 'double-well';  % use double-well potential in Eq. (16), which is good for both edge and region based models
+else
+    potentialFunction = 'double-well';  % default choice of potential function
+end
+
+
+% start level set evolution
+for n=1:iter_outer
+    phi = drlse_edge(phi, g, lambda, mu, alfa, epsilon, timestep, iter_inner, potentialFunction);
+    if mod(n,2)==0
+        imagesc(im,[0, 255]); axis off; axis equal; colormap(gray); hold on;  contour(phi, [0,0], 'r');
+    end
+end
+
+% refine the zero level contour by further level set evolution with alfa=0
+alfa=0;
+iter_refine = 10;
+phi = drlse_edge(phi, g, lambda, mu, alfa, epsilon, timestep, iter_inner, potentialFunction);
+
+finalLSF=phi;
+
+imagesc(Img,[0, 255]); axis off; axis equal; colormap(gray); hold on;  contour(phi, [0,0], 'r');
+hold on;  contour(phi, [0,0], 'r');
+str=['Final zero level contour, ', num2str(iter_outer*iter_inner+iter_refine), ' iterations'];
+title(str);
+
+pause(1);
+
+mesh(-finalLSF); % for a better view, the LSF is displayed upside down
+hold on;  contour(phi, [0,0], 'r','LineWidth',2);
+str=['Final level set function, ', num2str(iter_outer*iter_inner+iter_refine), ' iterations'];
+title(str);
+axis on;
